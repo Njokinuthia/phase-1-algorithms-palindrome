0eaea69c82ebf6cb70c9ba8d6da08f3a85c2920a
Njoki <kinuthiash@gmail.com>
2022-04-25 12:38:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // function isPalindrome
+  // takes in string (input)
+  //checks if letters forward make the same string reversed - return true
+  //returns boolean - true for same comparison , false for otherwise
+  
 }
 
 /* 
   Add your pseudocode here
+  // function palindrome(string) {
+    convert string to array
+  checks characters going forwwards 
+  checks reversed characters
+  compares the two strings
+  returns boolean 
+  }
 */
 
 /*
   Add written explanation of your solution here
 */
+
+function isPalindrome(string){
+  let reversedString = string.split('').reverse().join("")  
+  // console.log(reversedString)
+  return string === reversedString
+  // console.log(string === reversedString)
+ 
+}
+isPalindrome("racecar")
+isPalindrome("robot")
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
